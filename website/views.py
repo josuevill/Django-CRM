b0de33b9d7ba9bf503a8e2b9028e6eff84bdc768
josuevill <josuevill003@outlook.com>
2023-06-12 11:46:51 -0500

    creación de login y logout

--- website/views.py
+++ website/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Ingreso correcto")
+            return redirect('home')
+        else:
+            messages.success(request, "Ingreso de usuario y/o contraseña incorrecto, intente nuevamente")
+            return redirect('home')
+    else:    
+        return render(request, 'home.html', {})
+
+#def login_user(request):
+#    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Sesión cerrada")
+    return redirect('home')
+
